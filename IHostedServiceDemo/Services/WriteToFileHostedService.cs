5d5328aedfcf6939b21197daafbfade08fde537f
leo7962 <leonardohernandez.fp@academia.umb.edu.co>
2019-10-06 21:26:58 -0500

    Agregar archivos de proyecto.

---
+++ IHostedServiceDemo/Services/WriteToFileHostedService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService : IHostedService
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly string fileName = "File 1.txt";
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
