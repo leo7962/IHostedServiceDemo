c52ea25cf6b909e8193db60484b7fd3cf497c1d6
Leonardo Fabián Hernández Peña <leonardohernandez.fp@academia.umb.edu.co>
2019-10-06 23:43:47 -0500

    Se deja un servicio unitario, queda pendiente para montar el segundo servicio.

--- IHostedServiceDemo/Services/WriteToFileHostedService.cs
+++ IHostedServiceDemo/Services/WriteToFileHostedService.cs
@@ -1,25 +1,55 @@
 ﻿using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace IHostedServiceDemo.Services
 {
-    public class WriteToFileHostedService : IHostedService
+    public class WriteToFileHostedService : IHostedService, IDisposable
     {
-        private readonly IHostingEnvironment environment;
+        private readonly IHostEnvironment environment;
         private readonly string fileName = "File 1.txt";
+        private Timer timer;
+
+        public WriteToFileHostedService(IHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            WriteToFile("WriteToFileHostedService: Process Started");
+            timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            WriteToFile("WriteToFileHostedService: Doing some work at " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss"));
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            WriteToFile("WriteToFileHostedService: Process Stopped");
+            timer?.Change(Timeout.Infinite, 0);
+            return Task.CompletedTask;
+        }
+
+        private void WriteToFile(string message)
+        {
+            var path = $@"{environment.ContentRootPath}\wwwroot\{fileName}";
+            using (StreamWriter writer = new  StreamWriter(path, append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
         }
     }
 }
