c2e21bd6f34d51f2fa88e5e1e148640dd30fcb0a
Leonardo Fabián Hernández Peña <leonardohernandez.fp@academia.umb.edu.co>
2019-10-07 19:28:07 -0500

    Se crea una nueva clase que será un servicio

---
+++ IHostedServiceDemo/Services/WriteToFileHostedService2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService2
+    {
+    }
+}
